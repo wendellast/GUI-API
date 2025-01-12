45acc7210024c3ef274902a4828093fc1f04ddd2
wendellast <wendelalves01a@hotmail.com>
2025-01-12 01:16:33 -0300

    commit ia

--- main.py
+++ main.py
@@ -44,6 +44,22 @@ def send_message_portfolio():
 
     return jsonify({"response": result})
 
+@app.route('/send-message-gui-commitia', methods=['POST'])
+def send_message_commitia():
+    data = request.get_json()
+    message = data.get('message')
+
+    client = Client("wendellast/CommitIa")
+    result = client.predict(
+        message=message,
+        max_tokens=512,
+        temperature=0.7,
+        top_p=0.95,
+        api_name="/chat"
+    )
+
+    return jsonify({"response": result})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
